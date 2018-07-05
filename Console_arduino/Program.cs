35401cf177e62b1d87db42ddbb504e96bf583f52
José Humberto Abril <jhabril@gmail.com>
2018-07-04 19:47:29 -0700

    Imporrtin proyecct

---
+++ Console_arduino/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Ports;
+
+namespace Console_arduino
+{
+    class Program
+    {       
+
+        static void Main(string[] args)
+        {
+            SerialPort port = new SerialPort();
+            port.BaudRate=9600;
+            port.PortName = "COM3";
+            port.Open(); 
+       
+            port.Write("a");  
+
+            port.Close();
+
+
+            
+        }
+    }
+}
